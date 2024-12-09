879ef3dc77b63f07c7ee364a1bf1f4b8d25654d0
priyash7 <priyashshah4@gmail.com>
2024-12-09 16:56:25 +0530

    testing github workflows

--- .github/workflows/bump_version.py
+++ .github/workflows/bump_version.py
@@ -1,13 +1,14 @@
 import sys
 
 def main():
-    commit_message = sys.argv[1]  # First argument is the commit message
-    changed_files = sys.argv[2:]  # Remaining arguments are the changed files
+    # commit_message = sys.argv[1]  # First argument is the commit message
+    # changed_files = sys.argv[2:]  # Remaining arguments are the changed files
 
-    print(f"Commit Message: {commit_message}")
-    print("Changed Files:")
-    for file in changed_files:
-        print(f" - {file}")
+    print(sys.argv)
+    # print(f"Commit Message: {commit_message}")
+    # print("Changed Files:")
+    # for file in changed_files:
+    #     print(f" - {file}")
 
 if __name__ == "__main__":
     main()
