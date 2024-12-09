6af4c208238ef0c184ae74d0c60f00c2e89f3ded
priyash7 <priyashshah4@gmail.com>
2024-12-09 15:30:03 +0530

    uv basic setup

---
+++ operators/modulator/src/modulator/modulator.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Modulator:
+
+    def __init__(self, signal: np.ndarray, carrier: np.ndarray):
+        self.signal = signal
+        self.carrier = carrier
+
+    def am_modulate(self)->np.ndarray:
+        return self.signal * self.carrier
+
+    def fm_modulate(self)->np.ndarray:
+        return np.sin(self.signal + self.carrier)
+
+    def pm_modulate(self)->np.ndarray:
+        return np.cos(self.signal + self.carrier)
---
+++ src/verbump/verbump.py
@@ -0,0 +1,22 @@
+import numpy as np 
+
+class Calculator:
+
+    def __init__(a,b):
+        self.a = a
+        self.b = b
+    
+    def add(self)->int:
+        return self.a + self.b
+
+    def subtract(self)->int:
+        return self.a - self.b
+
+    def multiply(self)->int:
+        return self.a * self.b
+
+    def divide(self)->float:
+        return self.a / self.b
+
+    def power(self)->int:
+        return self.a ** self.b
