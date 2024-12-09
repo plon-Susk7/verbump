7fa423c9576efc1405c25d8d598e95828ff1c717
priyash7 <priyashshah4@gmail.com>
2024-12-09 15:29:34 +0530

    basic uv structure

---
+++ operators/power/src/power/power.py
@@ -0,0 +1,15 @@
+
+class Power:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def power(self):
+        return self.a ** self.b
+
+    def __str__(self):
+        return str(self.power())
+
+    def __repr__(self):
+        return str(self.power())
