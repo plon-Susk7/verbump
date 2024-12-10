b21c79279930cfd8728e6cef0072473f7366867f
priyash7 <priyashshah4@gmail.com>
2024-12-10 17:47:47 +0530

    feat: changed workflow

--- .github/workflows/bump_version.py
+++ .github/workflows/bump_version.py
@@ -4,40 +4,41 @@ import os
 
 
 def main():
-    commit_message = sys.argv[1]
-    changed_files = sys.argv[2].split(' ')
+    # commit_message = sys.argv[1]
+    # changed_files = sys.argv[2].split(' ')
+    print(sys.argv)
+    # print(changed_files)
+    # print(commit_message)
+    # commit_type = commit_message.split(':')[0].strip()
 
-    print(changed_files)
-    commit_type = commit_message.split(':')[0].strip()
-
-    for changed_file in changed_files:
-        splitting_paths = changed_file.split('/')
-        final_path = '/'.join(splitting_paths[0:-1]) + '/pyproject.toml'
-        print(final_path)
-        if os.path.exists(final_path):
-            with open(final_path,'r') as file:
-                data = toml.load(file)
+    # for changed_file in changed_files:
+    #     splitting_paths = changed_file.split('/')
+    #     final_path = '/'.join(splitting_paths[0:-1]) + '/pyproject.toml'
+    #     print(final_path)
+    #     if os.path.exists(final_path):
+    #         with open(final_path,'r') as file:
+    #             data = toml.load(file)
                 
-                if commit_type == 'feat':
-                    version = data['project']['version']
-                    version_split = version.split('.')
-                    version_split[0] = str(int(version_split[0]) + 1)
-                    data['project']['version'] = '.'.join(version_split)
-                elif commit_type == 'fix':
-                    version = data['project']['version']
-                    version_split = version.split('.')
-                    version_split[1] = str(int(version_split[1]) + 1)
-                    data['project']['version'] = '.'.join(version_split)
-                elif commit_type == 'chore':
-                    version = data['project']['version']
-                    version_split = version.split('.')
-                    version_split[2] = str(int(version_split[2]) + 1)
-                    data['project']['version'] = '.'.join(version_split)
+    #             if commit_type == 'feat':
+    #                 version = data['project']['version']
+    #                 version_split = version.split('.')
+    #                 version_split[0] = str(int(version_split[0]) + 1)
+    #                 data['project']['version'] = '.'.join(version_split)
+    #             elif commit_type == 'fix':
+    #                 version = data['project']['version']
+    #                 version_split = version.split('.')
+    #                 version_split[1] = str(int(version_split[1]) + 1)
+    #                 data['project']['version'] = '.'.join(version_split)
+    #             elif commit_type == 'chore':
+    #                 version = data['project']['version']
+    #                 version_split = version.split('.')
+    #                 version_split[2] = str(int(version_split[2]) + 1)
+    #                 data['project']['version'] = '.'.join(version_split)
             
-            with open(final_path, 'w') as file:
-                toml.dump(data, file)
+    #         with open(final_path, 'w') as file:
+    #             toml.dump(data, file)
 
-    print(f"Commit Type: {commit_type}")
+    # print(f"Commit Type: {commit_type}")
 
 if __name__ == "__main__":
     main()
